0472604df152313c9025303d015e9397edbf8ffe
Meagan Waller <meaganewaller@gmail.com>
2021-01-13 22:25:07 -0500

    🎨 Adds Recipes#create and validation for presence of recipe title

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,4 +11,19 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render action: "new"
+    end
+  end
+
+  protected
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  validates_presence_of :title
 end
