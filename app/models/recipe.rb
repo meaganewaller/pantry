878cb6e5203e2054d43b7d2cf5f96a34b9992105
Meagan Waller <meaganewaller@gmail.com>
2021-01-16 20:03:15 -0500

    ✨ Create or find ingredients when creating recipe ingredients to avoid duplicate ingredient names

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,12 @@ class Recipe < ApplicationRecord
   validates_presence_of :title
 
   accepts_nested_attributes_for :recipe_ingredients, allow_destroy: true
+
+  before_save :find_or_create_ingredients
+
+  def find_or_create_ingredients
+    self.recipe_ingredients.each do |recipe_ingredient|
+      recipe_ingredient.ingredient = Ingredient.find_or_create_by(name: recipe_ingredient.ingredient.name)
+    end
+  end
 end
